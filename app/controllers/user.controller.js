75edc2803cab232fd69efae9f13efc8053390dca
sonthuol <sonthuolb3@gmail.com>
2022-03-25 01:05:28 +0700

    update delete of account user

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -34,3 +34,76 @@ exports.getAllUser = async (req, res) => {
     });
   }
 };
+
+exports.details = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { id: req.params.id },
+      attributes: {
+        exclude: ["password"],
+      },
+    });
+    if (!user) {
+      res.status(500).send({
+        status: 500,
+        message: "Người dùng không tồn tại ",
+        data: [],
+      });
+    } else {
+      res.status(200).send({
+        status: 200,
+        message: "Người dùng tồn tại",
+        data: user,
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      status: 500,
+      message: "Người dùng không tồn tại " + error.message,
+      data: [],
+    });
+  }
+};
+
+//Sửa user
+exports.update = async (req, res) => {
+  try {
+    const user = await User.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).send({
+      status: 200,
+      message: "Cập nhật tài khoản thành công",
+    });
+  } catch (error) {
+    res.status(500).send({
+      status: 500,
+      message: "Cập nhật thành công không thành công",
+    });
+  }
+};
+
+//Xoá user
+exports.delete = async (req, res) => {
+  try {
+    const user = await User.update(
+      { isDelete: 1, deleteBy: req.body.user },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).send({
+      status: 200,
+      message: "Xoá tài khoản thành công",
+    });
+  } catch (error) {
+    res.status(500).send({
+      status: 500,
+      message: "Xoá tài khoản không thành công",
+    });
+  }
+};
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -12,44 +12,70 @@ module.exports = function (app) {
   //URL:/api/user/get-all-user
   app.get("/api/user/get-all-user", controller.getAllUser);
 
-  // //Ai cũng có quyền truy cập
-  // //method: GET
-  // //URL:/api/test/all
-  // app.get("/api/test/all", controller.allAccess);
+  //Ai cũng có quyền truy cập
+  //method: GET
+  //URL:/api/user/get-all-user
+  app.get("/api/user/:id", controller.details);
 
-  // //Truy cập đối với vai trò root
-  // //method: GET
-  // //URL: /api/test/root
-  // app.get(
-  //   "/api/test/root",
-  //   [authJwt.verifyToken, authJwt.isRoot],
-  //   controller.rootBoard
-  // );
+  //Cập nhật phòng khám
+  //method: PUT
+  //Access: Root, Admin
+  //URL:/api/clinics/:id
+  app.put(
+    "/api/user/:id",
+    // [authJwt.verifyToken, authJwt.isRootAdmin],
+    controller.update
+  );
 
-  // //Truy cập đối với vai trò admin
-  // //method: GET
-  // //URL: /api/test/admin
-  // app.get(
-  //   "/api/test/admin",
-  //   [authJwt.verifyToken, authJwt.isAdmin],
-  //   controller.adminBoard
-  // );
-
-  // //Truy cập đối với vai trò doctor
-  // //method: GET
-  // //URL: /api/test/admin
-  // app.get(
-  //   "/api/test/doctor",
-  //   [authJwt.verifyToken, authJwt.isDoctor],
-  //   controller.doctorBoard
-  // );
-
-  // //Truy cập đối với vai trò receptionst
-  // //method: GET
-  // //URL: /api/test/admin
-  // app.get(
-  //   "/api/test/receptionist",
-  //   [authJwt.verifyToken, authJwt.isReceptionist],
-  //   controller.receptionstBoard
-  // );
+  //Xoá phòng khám (Cập nhẩ giá trị isDelete)
+  //method: PATCH
+  //Access: Root
+  //URL:/api/clinics/:id
+  app.patch(
+    "/api/uses/:id",
+    // [authJwt.verifyToken, authJwt.isRoot],
+    controller.delete
+  );
 };
+
+// //Ai cũng có quyền truy cập
+// //method: GET
+// //URL:/api/test/all
+// app.get("/api/test/all", controller.allAccess);
+
+// //Truy cập đối với vai trò root
+// //method: GET
+// //URL: /api/test/root
+// app.get(
+//   "/api/test/root",
+//   [authJwt.verifyToken, authJwt.isRoot],
+//   controller.rootBoard
+// );
+
+// //Truy cập đối với vai trò admin
+// //method: GET
+// //URL: /api/test/admin
+// app.get(
+//   "/api/test/admin",
+//   [authJwt.verifyToken, authJwt.isAdmin],
+//   controller.adminBoard
+// );
+
+// //Truy cập đối với vai trò doctor
+// //method: GET
+// //URL: /api/test/admin
+// app.get(
+//   "/api/test/doctor",
+//   [authJwt.verifyToken, authJwt.isDoctor],
+//   controller.doctorBoard
+// );
+
+// //Truy cập đối với vai trò receptionst
+// //method: GET
+// //URL: /api/test/admin
+// app.get(
+//   "/api/test/receptionist",
+//   [authJwt.verifyToken, authJwt.isReceptionist],
+//   controller.receptionstBoard
+// );
+// };
