fe9f150a241d078eadfee75b059ed6ec85de0b71
sonthuol <sonthuolb3@gmail.com>
2022-05-15 15:48:17 +0700

    upload image

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ require("dotenv").config();
 //use
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(
   cookieSession({
     name: "tcare-session",
